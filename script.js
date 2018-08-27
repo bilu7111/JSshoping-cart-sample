3b23792740b20dd6274bf0722b4b97c6469f1dda
Bilal Kazmi <39033434+bilu7111@users.noreply.github.com>
2018-08-27 13:17:22 +0500

    Add files via upload
    Corrected some mistakes in script.js and updates content on DOM being loaded..

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-/*Referencing to Form with ID*/
+//Referencing to Form with ID//
 var cart=document.getElementById("cart");
-/*Referencing to box that shows the calculated result*/
+//Referencing to box that shows the calculated result//
 var total=document.getElementById("estimate");
-/*Referencing to box that shows price for each product*/
+//Referencing to box that shows price for each product//
 var rate=document.getElementsByClassName("rate");
-/*Making the price object constant so it can't be altered externally*/
-const price=[25,29.99,19.99,24.99];
+
+//Making the price object constant so it can't be altered externally//
+const price=[25,29.99,19,24.99];
 /*Showing the price for each product in it's respective price box*/
+document.addEventListener("DOMContentLoaded",function(){
 price.map(function(p,i){
-/*Notice I use the index of price object, as the length of price is the same as of rate length*/
-/*Leaving price for any product may cause an error*/
-rate[i].innerHTML="Price:"+p+"$";
+/*Notice I use the index of price object, as the length of price is the same as of rate length
+Leaving price for any product may cause an error*/
+rate[i].innerHTML="Price:"+p.toFixed(2)+"$";
 }
 );
-/*Adding event listener to form*/
+//Adding event listener to form//
 cart.addEventListener("submit",function(event){
-/*Make the estimate equal to zero so last calculated result is not included*/
+//Make the estimate equal to zero so last calculated result is not included//
 let estimate=0;
-/*Preventing the default browser refresh behaviour of form upon submission*/
+//Preventing the default browser refresh behaviour of form upon submission//
 event.preventDefault();
 /*Remember, the whole form element also contains a submit button so that's why
 [[i<(cart.length-1)]]*/
-for(i=0;i<(cart.length-1);i++){
+for(i=0;i<(cart.length-2);i++){
   let temp=parseFloat(cart[i].value*price[i]);
   estimate=temp+estimate;
 }
-/*Changing the html of total estimation box to calculated result*/
+//Changing the html of total estimation box to calculated result//
 total.innerHTML=estimate.toFixed(2)+"$";
+});
 });
